291d471a090f994cd31f761d7ad6135d481359ee
AbhiG5771 <abhijitgulhane009@gmail.com>
2023-08-06 17:49:00 -0700

    [AbhiG5771] ADD New Proj

---
+++ src/test/java/clothingStore/Raymond.java
@@ -0,0 +1,18 @@
+package clothingStore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Raymond {
+  @Test
+  public void RaymondTest() throws InterruptedException 
+  {
+	  WebDriver driver = new ChromeDriver ();
+		 Reporter.log("opening Raymond", true);
+		 driver.get("https://www.raymond.in/");
+		 Thread.sleep(5000);
+		 driver.close();
+  }
+}
